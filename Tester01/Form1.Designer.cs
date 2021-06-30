b17f3b06abe5cc36c2fa187a47d09df2bf2cfc31
mehmettahiralbayrak@hotmail.com <mehmettahiralbayrak@hotmail.com>
2021-06-30 08:53:32 +0300

    Proje dosyası ekle.

---
+++ Tester01/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace Tester01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ScanofPorts = new System.Windows.Forms.Timer(this.components);
+            this.VaccPort = new System.IO.Ports.SerialPort(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.pbMain = new System.Windows.Forms.ToolStripProgressBar();
+            this.tsLblMain = new System.Windows.Forms.ToolStripStatusLabel();
+            this.lblSSID = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSsıdGet = new System.Windows.Forms.Button();
+            this.btnSsıdSet = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ScanofPorts
+            // 
+            this.ScanofPorts.Interval = 1000;
+            this.ScanofPorts.Tick += new System.EventHandler(this.SearchVaccineRef);
+            // 
+            // VaccPort
+            // 
+            this.VaccPort.BaudRate = 115200;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pbMain,
+            this.tsLblMain});
+            this.statusStrip1.Location = new System.Drawing.Point(812, 537);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(205, 26);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // pbMain
+            // 
+            this.pbMain.Name = "pbMain";
+            this.pbMain.Size = new System.Drawing.Size(100, 18);
+            // 
+            // tsLblMain
+            // 
+            this.tsLblMain.Name = "tsLblMain";
+            this.tsLblMain.Size = new System.Drawing.Size(86, 20);
+            this.tsLblMain.Text = "lMainStatus";
+            // 
+            // lblSSID
+            // 
+            this.lblSSID.AutoSize = true;
+            this.lblSSID.Location = new System.Drawing.Point(25, 63);
+            this.lblSSID.Name = "lblSSID";
+            this.lblSSID.Size = new System.Drawing.Size(39, 17);
+            this.lblSSID.TabIndex = 1;
+            this.lblSSID.Text = "SSID";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnSsıdGet
+            // 
+            this.btnSsıdGet.Location = new System.Drawing.Point(88, 120);
+            this.btnSsıdGet.Name = "btnSsıdGet";
+            this.btnSsıdGet.Size = new System.Drawing.Size(167, 23);
+            this.btnSsıdGet.TabIndex = 3;
+            this.btnSsıdGet.Text = "GET";
+            this.btnSsıdGet.UseVisualStyleBackColor = true;
+            // 
+            // btnSsıdSet
+            // 
+            this.btnSsıdSet.Location = new System.Drawing.Point(88, 91);
+            this.btnSsıdSet.Name = "btnSsıdSet";
+            this.btnSsıdSet.Size = new System.Drawing.Size(167, 23);
+            this.btnSsıdSet.TabIndex = 3;
+            this.btnSsıdSet.Text = "SET";
+            this.btnSsıdSet.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1055, 562);
+            this.Controls.Add(this.btnSsıdSet);
+            this.Controls.Add(this.btnSsıdGet);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblSSID);
+            this.Controls.Add(this.statusStrip1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer ScanofPorts;
+        private System.IO.Ports.SerialPort VaccPort;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar pbMain;
+        private System.Windows.Forms.ToolStripStatusLabel tsLblMain;
+        private System.Windows.Forms.Label lblSSID;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSsıdGet;
+        private System.Windows.Forms.Button btnSsıdSet;
+    }
+}
+
---
+++ Tester01/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+
+
+
+
+namespace Tester01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ScanofPorts.Enabled = true;
+        }
+
+        enum Commands
+        {
+            HostPrefixA     = 0xc2,
+            HostPrefixB     = 0xb1,
+            DevicePrefixA   = 0x81,
+            DevicePrefixB   = 0x12,
+
+            UsbCmd = 0xA6,
+            UsbGetStatus = 0x11
+        }
+
+        enum Cmd
+        {
+            GetVersion = 0,
+            GetStatus
+        }
+
+        UInt32 Crc32(UInt32 Crc, UInt32 Data)
+        {
+            int i;
+            Crc = Crc ^ Data;
+            for (i = 0; i < 32; i++)
+                if ((Crc & 0x80000000) == 0x80000000)
+                    Crc = (Crc << 1) ^ 0x04C11DB7; // Polynomial used in STM32
+                else
+                    Crc = (Crc << 1);
+            return (Crc);
+        }
+
+        byte[] CalcCRC(byte[] data, int length)
+        {
+            byte[] crc_data = new byte[length - 2];
+            int crc_length = length - 2;
+
+            Array.Copy(data, 2, crc_data, 0, crc_length);
+
+            UInt32 Crc = 0xFFFFFFFF;
+            UInt32 i_data;
+            for (int i = 0; i < crc_length / 4; ++i)
+            {
+                i_data = BitConverter.ToUInt32(crc_data, i * 4);
+                Crc = Crc32(Crc, i_data);
+            }
+            return (BitConverter.GetBytes(Crc));
+        }
+
+        bool GetCommand(SerialPort sp, Commands cmd, Commands subCmd, out int length,out byte[] data)
+        {
+            data = new byte[1]; length = 0;
+            try
+            {
+               data = new byte[1];
+
+               length = sp.BytesToRead;
+
+                byte[] rsp = new byte[length];
+                if (sp.Read(rsp, 0, length) < 9) return false;
+
+                for (int i = 0; i < length - 2; i++)
+                {
+                    if ((rsp[i] == 0x81) & (rsp[i + 1] == 0x12))
+                    {
+                        if (rsp[i + 2] == (byte)cmd)
+                        {
+                            if (rsp[i + 3] == (byte)subCmd)
+                            {
+                                length = rsp[i+4]; data = new byte[length];
+                                Array.Copy(rsp, i + 5, data, 0, length);
+                                return true;
+                            }
+                        }
+                    }
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        bool SendCommand(SerialPort sp,Commands cmd,Commands subCmd,byte length,byte[] parameters)
+        {
+            try
+            {
+                byte[] sendBuffer = new byte[length + 9];
+                sendBuffer[0] = (byte)Commands.HostPrefixA;
+                sendBuffer[1] = (byte)Commands.HostPrefixB;
+                sendBuffer[2] = (byte)cmd;
+                sendBuffer[3] = (byte)subCmd;
+                sendBuffer[4] = length;
+                if(parameters.Length >= length)
+                    Array.Copy(parameters, 0, sendBuffer,5,length);
+
+                byte[] crc = CalcCRC(sendBuffer, 5+length);
+
+                Array.Copy(crc, 0, sendBuffer, 5+length, 4);
+
+                sp.Write(sendBuffer, 0, sendBuffer.Length);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public string GetVersion(SerialPort sp)
+        {
+            try
+            {
+                SendCommand(sp, Commands.UsbCmd, Commands.UsbGetStatus, 0, new byte[0]);
+                
+                System.Threading.Thread.Sleep(500);
+
+                int length = 0; byte[] rsp;
+                if (GetCommand(sp, Commands.UsbCmd, Commands.UsbGetStatus, out length, out rsp))
+                {
+                    string Version = $"Version : {rsp[2].ToString()}.{rsp[1].ToString()}";
+                    return Version;
+                }
+                return "";
+            }
+            catch (Exception ex)
+            {
+                return "";
+            }
+        }
+
+        string vaccinerefport = "";
+        private void SearchVaccineRef(object sender, EventArgs e)
+        {
+
+            string[] ports = SerialPort.GetPortNames();
+            if (ports.Length>0)
+            {
+                foreach (string port in ports)
+                {
+                    if (port == vaccinerefport)
+                    {
+                        pbMain.Value = 100;
+                        return;
+                    }
+                    pbMain.Value = 0;
+                    VaccPort = new SerialPort(port,115200,Parity.None,8,StopBits.One);
+                    VaccPort.Open();
+                    if (VaccPort.IsOpen)
+                    {
+                        tsLblMain.Text = GetVersion(VaccPort);
+                        if (tsLblMain.Text != "")
+                        {
+                            pbMain.Value = 100;
+                            vaccinerefport = port;
+                        }
+                    }
+                    VaccPort.Close();
+                }
+            }
+            else
+            {
+                tsLblMain.Text = "Trying to connect to refrigereator...";
+                pbMain.Value = 0;
+                vaccinerefport = "";
+            }
+        }
+    }
+}
